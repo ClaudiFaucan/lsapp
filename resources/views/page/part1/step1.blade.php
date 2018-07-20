90a64ce6e7a2bf3f8818686101a21fabdf740a1d
claudiefaucan <raphaellarranaga@gmail.com>
2018-07-20 17:31:14 +0200

    last

--- resources/views/page/part1/step1.blade.php
+++ resources/views/page/part1/step1.blade.php
@@ -1602,6 +1602,7 @@
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
+                        <label>Affinez votre demande de crédit avec un expert Tolmar</label>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
                         <h4 class="modal-title" id="myModalLabel"></h4>
                     </div>
